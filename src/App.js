4fa69091a42ab449c2410154b94b876b98cf234a
Hellen <hellenfc93@gmail.com>
2021-01-03 23:18:43 -0600

    Router changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="root">
-        <Router basename="/">
+        <Router basename="/playlist-test">
           <Header></Header>
           <Switch>
             <Route exact
